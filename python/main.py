2c9c63798c9db4baa6313639119b37d87ece8092
belyaevEDU <veniamin@belyaev.work>
2026-05-12 19:15:33 +0300

    fix(python/main): added no body notice & a to-do note for 207 handling

--- python/main.py
+++ python/main.py
@@ -5,12 +5,15 @@ def handleCode(response: requests.Response) -> None:
 
     if 100 <= code and code < 400:
         print(f"Status code: {code}")
-        print(f"Body: {response.content.decode()}\n")
+        body = response.content.decode()
+        if body == "":
+            body = "<No body>"
+        print(f"Body: {body}\n")
     elif 400 <= code and code < 600:
         raise Exception(f"Exception raised. Status code: {code}\n")
 
 # В соответствии документации httpstat
-POSSIBLE_STATUS_CODES = "100-103,200-208,226,300-308,400-426,427-428,431,440,449-451,460,463,494-499,500-508,510-511"
+POSSIBLE_STATUS_CODES = "100-103,200-208,226,300-308,400-426,427-428,431,440,444,449-451,460,463,494-499,500-508,510-511" # Work out a way to handle 207
 # Оригинальный сервис отключили из-за наплыва трафика (ссылка в README), поэтому использую зеркало
 BASE_URL = "https://tools-httpstatus.pickup-services.com/random/"
 
