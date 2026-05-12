3854f0252c1fc9608d7422df5114724a63549bf5
belyaevEDU <veniamin@belyaev.work>
2026-05-12 18:53:01 +0300

    docs(python/main): added explanations for constant strings

--- python/main.py
+++ python/main.py
@@ -9,8 +9,9 @@ def handleCode(response: requests.Response) -> None:
     elif 400 <= code and code < 600:
         raise Exception(f"Exception raised. Status code: {code}\n")
 
-
+# В соответствии документации httpstat
 POSSIBLE_STATUS_CODES = "100-103,200-208,226,300-308,400-426,427-428,431,440,449-451,460,463,494-499,500-508,510-511"
+# Оригинальный сервис отключили из-за наплыва трафика (ссылка в README), поэтому использую зеркало
 BASE_URL = "https://tools-httpstatus.pickup-services.com/random/"
 
 r = requests.get(BASE_URL + POSSIBLE_STATUS_CODES)
