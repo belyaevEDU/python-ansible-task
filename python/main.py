32c924d8c6aee5b733677d2b969bcee9edd5810b
belyaevEDU <veniamin@belyaev.work>
2026-05-12 20:21:14 +0300

    style(python/main): changed logging's format to be more concise and readable

--- python/main.py
+++ python/main.py
@@ -1,7 +1,7 @@
 import requests
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 logger = logging.getLogger(__name__)
 
 
