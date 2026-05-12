9d825985c72c94fa0959dd3cd3a0b1959feafd28
belyaevEDU <veniamin@belyaev.work>
2026-05-12 20:11:16 +0300

    fix(python/main): added flag to not allow redirects so that redirect/moved codes work & replaced my error clause with raise for status

--- python/main.py
+++ python/main.py
@@ -1,6 +1,8 @@
 import requests
 
 def handleCode(response: requests.Response) -> None:
+    response.raise_for_status()
+
     code = response.status_code
 
     if 100 <= code and code < 400:
@@ -9,16 +11,16 @@ def handleCode(response: requests.Response) -> None:
         if body == "":
             body = "<No body>"
         print(f"Body: {body}\n")
-    elif 400 <= code and code < 600:
-        raise Exception(f"Exception raised. Status code: {code}\n")
 
-# В соответствии документации httpstat
-POSSIBLE_STATUS_CODES = "100-103,200-208,226,300-308,400-426,427-428,431,440,444,449-451,460,463,494-499,500-508,510-511" # Work out a way to handle 207
 # Оригинальный сервис отключили из-за наплыва трафика (ссылка в README), поэтому использую зеркало
 BASE_URL = "https://tools-httpstatus.pickup-services.com/random/"
 
+# В соответствии документации httpstatus
+# Исключен код 100 Continue из-за известной лимитации http.client внутри питона, из-за которого программа зависает при встрече этого кода статуса
+POSSIBLE_STATUS_CODES = "101-103,200-208,226,300-308,400-426,427-428,431,440,444,449-451,460,463,494-499,500-508,510-511"
+
 for _ in range(5):
-    r = requests.get(BASE_URL + POSSIBLE_STATUS_CODES)
+    r = requests.get(BASE_URL + POSSIBLE_STATUS_CODES, timeout=5, allow_redirects=False)
     try:
         handleCode(r)
     except Exception as e:
